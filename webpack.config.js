f5079f00bdcf4b8fb26e46037418270724412df2
Ain <ain.salula@gmail.com>
2017-02-07 19:16:12 +0200

    configure webpack & babel packages, add es6 features

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,18 @@ module.exports = {
     output: {
         filename: "bundle.js"
     },
-    watch: true
+    watch: true,
+
+    module: {
+        loaders: [
+            {
+                test: /\.es6$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.es6']
+    }
 }
