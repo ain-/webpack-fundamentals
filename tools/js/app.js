d35fa7ace306e84baa3f6c2eedfb6dc42c420d53
Ain <ain.salula@gmail.com>
2017-02-12 17:31:21 +0200

    creating global variables using plugins

--- tools/js/app.js
+++ tools/js/app.js
@@ -2,6 +2,8 @@ console.log('App loaded');
 
 var config = require('../config/config.json');
 
+$('#testDiv').text('jQuery modified this content(app.js)');
+console.log($('#testDiv').text);
 console.log(config.app_loaded_msg);
 
 require('../css/style.css');
